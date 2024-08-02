5c6188974b67d0089e32940f1ce10194de8fc0fc
Filip Molcik <filip.molcik@koala42.com>
2024-08-02 22:08:10 +0200

    chrome-extension add script

---
+++ chrome-extension/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  console.log("here we go")
+	// chrome.tabs.executeScript(null,{code:"document.getElementById('ap_customer_name').value = '"+varxhrjson['cname']+"'"});
+	
+});
